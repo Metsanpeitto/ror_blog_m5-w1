53b03d065844cd67e59fb7d55ff66af63174c947
Metsanpeitto <abraham@waldenberginc.com>
2021-10-26 01:27:25 +0200

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,4 @@
 class User < ApplicationRecord
   has_many :tasks, :comments, :posts
   validates :name, :photo, :bio, presence: true
-
-  def last_posts
-    byebug
-    posts = Post.find(:user).limit(3)
-    p posts
-  end
 end
