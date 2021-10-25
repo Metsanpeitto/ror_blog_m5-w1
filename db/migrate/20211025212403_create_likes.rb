fcdc0390417945089fa39839de4ccec8e778e5a0
Metsanpeitto <abraham@waldenberginc.com>
2021-10-26 00:26:52 +0200

    Add index to foreign keys

--- db/migrate/20211025212403_create_likes.rb
+++ db/migrate/20211025212403_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, index: true
+      t.references :post, null: false, foreign_key: true, index: true
 
       t.timestamps
     end
--- db/migrate/20211025213357_create_posts.rb
+++ db/migrate/20211025213357_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true,index: true
       t.string :title
       t.text :text
       t.integer :comments_counter
