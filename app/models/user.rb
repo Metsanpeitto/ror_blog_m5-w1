e1b01eba68ad19013d864d2d5f680e47813a7de5
Metsanpeitto <abraham@waldenberginc.com>
2021-10-26 01:15:28 +0200

    Add posts_counter to use in the schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :tasks, :comments, :posts
   validates :name, :photo, :bio, presence: true
+
+  def last_posts
+    byebug
+    posts = Post.find(:user).limit(3)
+    p posts
+  end
 end
