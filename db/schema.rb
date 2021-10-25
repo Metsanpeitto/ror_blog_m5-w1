18e069d9a1ad1843f87fc48fae4b1fdb52af8c8b
Metsanpeitto <abraham@waldenberginc.com>
2021-10-26 01:24:21 +0200

    Add posts counter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_221958) do
+ActiveRecord::Schema.define(version: 2021_10_25_232337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_10_25_221958) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_counter"
   end
 
   add_foreign_key "comments", "posts"
