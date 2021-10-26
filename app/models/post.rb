9d3d30711e6c286ae7212fc762346624498d9042
Metsanpeitto <abraham@waldenberginc.com>
2021-10-26 05:32:37 +0200

    Add methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments, :likes
+  has_many :comments
+  has_many :likes
   validates :title, :text, presence: true
+
+  def self.update_counter(user_id)
+    num_posts = Post.where(user_id: user_id).count
+    User.find_by(id: user_id).update(post_counter: num_posts)
+  end
+
+  def self.most_recent(post_id)
+    Comment.where(post_id: post_id).order(created_at: :desc).limit(5)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :posts
   get "/users/:user_id/posts", to: "posts#index" # usually requires a view
   get "/users/:user_id/posts/:id", to: "posts#show" # usually requires a view
-  get "/users", to: "users#index" # usually requires a view
-  get "/users/:id", to: "users#show" # usually requires a view  
 end
