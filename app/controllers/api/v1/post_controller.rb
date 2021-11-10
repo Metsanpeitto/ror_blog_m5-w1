c855e4d2672a791dbe17294b6657a1cc96857c9c
Metsanpeitto <abraham@waldenberginc.com>
2021-11-10 21:00:51 +0100

    Fix authorization

--- app/controllers/api/v1/post_controller.rb
+++ app/controllers/api/v1/post_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::PostController < ApiController
   before_action :authenticate_user!
   def index
     puts current_user.email
+    byebug
     render json: { user: current_user.email, list: [1, 2, 3] }
   end
 end
