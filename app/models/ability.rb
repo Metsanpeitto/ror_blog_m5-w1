0f9667d958092994f9d1b513952a7cf5e76490bc
Metsanpeitto <abraham@waldenberginc.com>
2021-11-11 23:52:56 +0100

    Fail merge

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,19 +3,18 @@ class Ability
 
   def initialize(user)
     user ||= User.new
+
     # Define User abilities
     if user.role == 'admin'
       can :manage, Post
       can :manage, Comment
 
-    elsif user.present?
+    else user.present?
       can :manage, Post, user: user
       can :manage, Comment, user: user
     else
       can :read, Post
       can :read, Comment
     end
-
-    can :manage, Post, Comment if user.present?
   end
 end
