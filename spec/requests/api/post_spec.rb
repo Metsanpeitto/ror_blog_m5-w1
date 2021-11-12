3308d90f5d68c01191aa95f503d040df877e6462
Metsanpeitto <abraham@waldenberginc.com>
2021-11-12 18:28:33 +0100

    Fix linters

--- spec/requests/api/post_spec.rb
+++ spec/requests/api/post_spec.rb
@@ -2,33 +2,23 @@ require 'swagger_helper'
 
 RSpec.describe 'api/post', type: :request do
   path '/api/v1/post/index' do
-    get 'Retrieves a blog' do
-      tags 'Blogs', 'Another Tag'
-      produces 'application/json', 'application/xml'
-      parameter name: :id, in: :path, type: :string
-
-      response '200', 'blog found' do
-        schema type: :object,
-               properties: {
-                 id: { type: :integer },
-                 title: { type: :string },
-                 content: { type: :string }
-               },
-               required: %w[id title content]
-
-        let(:id) { Post.all }
+    get 'Retrieves the posts' do
+      security [bearer_auth]
+      response 200, 'posts found' do
+        login_user
+        let(:Authorization) { "Bearer #{@user.jti}" }
         run_test!
       end
 
-      response '404', 'blog not found' do
-        let(:id) { 'invalid' }
-        run_test!
-      end
+      # response '404', 'blog not found' do
+      #  let(:id) { 'invalid' }
+      #  run_test!
+      # end
 
-      response '406', 'unsupported accept header' do
-        let(:Accept) { 'application/foo' }
-        run_test!
-      end
+      # response '406', 'unsupported accept header' do
+      #  let(:Accept) { 'application/foo' }
+      #  run_test!
+      # end
     end
   end
 end
